419b7fcd3df4c90e79e5a8f91ba61cdccd5082cf
L-VIT <orobey1997@gmail.com>
2022-06-30 18:00:01 +0300

    complited-home-screen

--- js/main.js
+++ js/main.js
@@ -4,17 +4,21 @@ $(function () {
         $(this).addClass('link__item--active');
     });
 
-    jQuery(document).ready(function ($) {
-        $('.copy_text').click(function () {
-            var $text_copy = $(this);
-            var $temp = $("<input>");
-            $("body").append($temp);
-            $temp.val($text_copy.text()).select();
-            document.execCommand("copy");
-            $temp.remove();
-            $('.copy_link_mess').fadeIn(400);
-            setTimeout(function () { $('.copy_link_mess').fadeOut(400); }, 5000);
-        });
+    $('.copy_text').click(function () {
+        var $text_copy = $(this);
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($text_copy.text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+        $('.copy_link_mess').fadeIn(400);
+        setTimeout(function () { $('.copy_link_mess').fadeOut(400); }, 5000);
+    });
+
+    $('.faq__list-item').on('click', function(){
+        $(this).toggleClass('faq__list-item--active');
+        $(this).children('.faq__list-content').children('.faq__list-title').toggleClass('faq__list-title--active');
+        $(this).children('.faq__list-content').children('.faq__list-title').next().slideToggle('200');
     });
 
 });
